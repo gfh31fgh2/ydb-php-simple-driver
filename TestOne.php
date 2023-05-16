f3a902b50c5561359b35e0a4c20459f2716d2dc9
gfh31fgh2 <gfh31fgh2>
2023-05-16 23:22:30 +0500

    adding examples of data_for_ydb

--- TestOne.php
+++ TestOne.php
@@ -16,12 +16,17 @@ class TestOne
     public function run()
     {
 
-        $test_arr = $this->ydb_v2->selectPythonYDB_v4('kkt_status', $data_for_ydb, 'equal', null, null);
+        $data_for_ydb = array(
+            'column1' => 'some_record_where_we_try_to_find'
+        );
+        $test_arr = $this->ydb_v2->selectPythonYDB_v4('any_table_in_table_scheme', $data_for_ydb, 'equal', null, null);
         $test = $test_arr[0] ?? "";
         var_dump(json_encode($test));
 
         // $this->response = !empty ($this->response) ? $this->response : '{"status":"error", "error":"xxxx", "code":"80"}';
 
+        // $data_for_ydb['column1'] = "text1 equal to type of column1 in scheme";
+        // $data_for_ydb['column2'] = "text2 equal to type of column2 in scheme";
         // $save_res = $this->ydb_v2->savePythonYDB('table2', $data_for_ydb);
         // var_dump(json_encode($save_res));
     }
